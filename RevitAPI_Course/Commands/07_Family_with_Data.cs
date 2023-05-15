d0263a5a7486e451ae07eee7b8a11ff1e2469bef
Turtle Wolfe <JonPohlner@gmail.com>
2023-05-15 12:53:44 -0400

    25. Extracting Data From Elements

---
+++ RevitAPI_Course/Commands/07_Family_with_Data.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.Attributes;
+using Autodesk.Revit.UI.Selection;
+using Autodesk.Revit.DB.Structure;
+namespace RevitAPI_Course
+{
+    [Transaction(TransactionMode.Manual)]
+    [Regeneration(RegenerationOption.Manual)]
+
+    public class _07_Family_with_Data : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            // Selection or Extraction
+            UIApplication uiapp = commandData.Application;
+            Document doc = uiapp.ActiveUIDocument.Document;
+            Element selected = Extraction.SingleElementSelection(uiapp);
+            ElementId familyTypeId = selected.GetTypeId();
+
+            FamilySymbol FamS = doc.GetElement(familyTypeId) as FamilySymbol;
+            Level lvl = doc.GetElement(selected.LevelId) as Level;
+            List<FamilySymbol> allColumnsFamilySymbols = Extraction.GetAllFamilySymbolsOfCategoryFamilyName(doc, BuiltInCategory.OST_StructuralColumns, "Concrete-Rectangular-Column");
+            //foreach(FamilySymbol FS in allColumnsFamilySymbols)
+            //           {
+            //    if (FS.FamilyName = FamS.FamilyName)
+            //    {
+            //        FamS = FS;
+            //        break;
+            //    }
+            //}
+
+            List<Level> allLevels = Extraction.GetAllLevelsFromModel(doc);
+            
+            // Creation
+            Transaction trans = new Transaction(doc);
+            trans.Start("Starting Process");
+            if (!allColumnsFamilySymbols[0].IsActive)
+            {
+                allColumnsFamilySymbols[0].Activate();
+                doc.Regenerate();
+            }
+            // Creation Process
+            FamilyInstance fam = doc.Create.NewFamilyInstance(new XYZ(0, 0, 0), FamS, lvl, StructuralType.Column);
+
+            trans.Commit();
+            return Result.Succeeded;
+        }       
+    }
+}
